a13580cf7a5cdb226e723d84b8e2152af2020c48
Chris McCormick <chris@mccormick.cx>
2014-06-07 23:09:35 +0200

    Round trip between console and Pd working.

---
+++ client/phonon.js
@@ -0,0 +1,34 @@
+var port = require('port');
+
+function Phonon(receive_function, connect_function, stderr_function) {
+	var pd_socket = null;
+	
+	port({
+	    'read': 8205,
+	    'write': 8206,
+	    // 'basepath': __dirname,
+	    'flags': {
+		'nogui': false,
+		'stderr': true,
+		'path': __dirname,
+		'open': 'patches/phonon_main.pd'
+	    }
+	})
+	.on('connect', function(socket) {
+		pd_socket = socket;
+		connect_function();
+	})
+	.on('data', receive_function)
+	.on('stderr', stderr_function)
+	.create();
+        
+	this.send = function(line) {
+		if (pd_socket) {
+			pd_socket.write(line + ";\n");
+		} else {
+			stderr_function("Pd not connected/running.");
+		}
+	}
+}
+
+module.exports = Phonon
